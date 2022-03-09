45db4ce59ac6fe3eb058968a0a3c9008003a6d92
luan fellipe <luanfelliipe8@gmail.com>
2022-03-09 01:10:03 -0300

    Add files via upload

---
+++ ReadME_Exemplo.md
@@ -0,0 +1,6 @@
+# Cotação de Moedas (Bitcoin e Dólar) para R$ em Tempo Real! (Desktop)
+### Autor: Luan Fellipe (allen08)
+O programa pode ser usado tanto individualmente (como APIs) ou na Interface Grafica (Desenvolvida no Python Tkinter)
+
+## [*] Requisitos:
+	- Python 3.4 ou Superior
---
+++ dolarCotacaoScript.py
@@ -0,0 +1,13 @@
+﻿import requests,json,time
+
+def cotacao_dolar():
+	dolar=requests.get('https://economia.awesomeapi.com.br/json/daily/USD-BRL')
+	if dolar.status_code!=200:
+		print('erro na requisição da api!'); exit()
+	cotacao_dolar=json.loads(dolar.text)
+	dolar_cotacao='Dolar Valor atual: %.2fR$'%float(cotacao_dolar[0]['high'])
+	return dolar_cotacao
+
+if __name__=='__main__':
+	print(cotacao_dolar())
+	time.sleep(10)
